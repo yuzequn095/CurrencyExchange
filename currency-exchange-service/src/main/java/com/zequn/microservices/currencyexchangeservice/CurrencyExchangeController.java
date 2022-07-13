255b9134eb0943a9b7cfa05b89a00fca06a1d0c1
yuzequn095 <yuzequn095@gmail.com>
2022-07-12 23:59:00 -0700

    The codes in this commit set up the hard coded service and database

---
+++ currency-exchange-service/src/main/java/com/zequn/microservices/currencyexchangeservice/CurrencyExchangeController.java
@@ -0,0 +1,27 @@
+package com.zequn.microservices.currencyexchangeservice;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.math.BigDecimal;
+
+@RestController
+public class CurrencyExchangeController {
+
+    @Autowired
+    private Environment environment;
+
+    @GetMapping("/currency-exchange/from/{from}/to/{to}")
+    public CurrencyExchange retrieveExchangeValue(@PathVariable String from, @PathVariable String to){
+        CurrencyExchange currencyExchange = new CurrencyExchange(1000L, from, to, BigDecimal.valueOf(6.7));
+
+        String port = environment.getProperty("local.server.port");
+        currencyExchange.setEnvironment(port);
+
+        return currencyExchange;
+    }
+
+}
---
+++ currency-exchange-service/src/main/resources/data.sql
@@ -0,0 +1,9 @@
+insert into currency_exchange
+(id,currency_from,currency_to,conversion_multiple,environment)
+values(10001,'USD','CNY',6.7,'');
+insert into currency_exchange
+(id,currency_from,currency_to,conversion_multiple,environment)
+values(10002,'USD','EUR',1.0,'');
+insert into currency_exchange
+(id,currency_from,currency_to,conversion_multiple,environment)
+values(10003,'USD','JPY',137.14,'');
